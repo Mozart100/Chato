afd820db04f12ec3a86f915cf50f9d0b5f1a20af
Arkovean <anatoliy.arkove@gmail.com>
2024-04-06 16:12:25 +0300

    Chato.Automation

---
+++ Chato.Automation/Scenario/HubScenarioBase.cs
@@ -0,0 +1,59 @@
+﻿using Chato.Server.Hubs;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Chato.Automation.Scenario;
+
+public abstract class HubScenarioBase : ScenarioBase
+{
+    protected const string Hub_Send_Message_Topic = nameof(ChatHub.SendMessageAllUsers);
+
+    protected Dictionary<string, UserHubChat> Users;
+    protected HubConnection Connection;
+
+    public HubScenarioBase(string baseUrl) : base(baseUrl)
+    {
+
+        Users = new Dictionary<string, UserHubChat>();
+
+        Connection = new HubConnectionBuilder()
+         .WithUrl(BaseUrl)
+         .WithAutomaticReconnect()
+         .Build();
+
+        Connection.Reconnecting += (sender) =>
+        {
+            Logger.Info("Connection reconnecting");
+            return Task.CompletedTask;
+        };
+
+        Connection.Reconnected += (sender) =>
+        {
+            Logger.Info($"Hub Connected.");
+            return Task.CompletedTask;
+
+        };
+
+        Listen();
+    }
+
+    private void Listen()
+    {
+        Connection.On<string, string>(ChatHub.TOPIC_MESSAGE_RECEIVED, (user, message) =>
+        {
+            foreach (var keyAndValue in Users)
+            {
+                if (user.Equals(keyAndValue.Key, StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    keyAndValue.Value.Received(user, message);
+                }
+            }
+        });
+    }
+
+    public async Task SendMessageToAllUSers(string userNameFrom, string message)
+    {
+        await Connection.SendAsync(Hub_Send_Message_Topic, userNameFrom, message);
+    }
+
+
+}
---
+++ Chato.Automation/Scenario/PingChatScenario.cs
@@ -0,0 +1,46 @@
+﻿using Chato.Automation.Scenario;
+
+namespace Arkovean.Chat.Automation.Scenario;
+
+internal class PingChatScenario : HubScenarioBase
+{
+    private readonly UserHubChat _user1;
+    private readonly UserHubChat _user2;
+
+    private SemaphoreSlim _signal;
+
+    public PingChatScenario(string baseUrl) : base(baseUrl)
+    {
+        _user1 = new UserHubChat(this, "anatoliy");
+        _user2 = new UserHubChat(this, "nathan");
+
+        Users.Add(_user1.Name, _user1);
+        Users.Add(_user2.Name, _user2);
+
+
+
+        _signal = new SemaphoreSlim(1);
+
+        BusinessLogicCallbacks.Add(PingHub);
+    }
+
+
+    public override string ScenarioName => "PingChatHub";
+
+    public override string Description => "Testing connectivity of th hub";
+
+    private async Task PingHub()
+    {
+        await Connection.StartAsync();
+
+        //await Connection.SendAsync(Hub_Send_Message_Topic, "xxx", "yyyy");
+
+        //await _user1.SendAsync("yyy");
+
+
+        //await _signal.WaitAsync(TimeSpan.FromSeconds(20));
+
+        await Task.Delay(TimeSpan.FromMinutes(20));
+    }
+
+}
