74d35dc634dc41b90516e029af1c4923e4c8e288
Arkovean <anatoliy.arkove@gmail.com>
2024-05-14 11:07:25 +0300

    selfcode review

--- Chato.Server/Controllers/WeatherForecastController.cs
+++ Chato.Server/Controllers/WeatherForecastController.cs
@@ -1,28 +1,28 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace Chato.Server.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+//namespace Chato.Server.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class WeatherForecastController : ControllerBase
+//    {
+//        private static readonly string[] Summaries = new[]
+//        {
+//        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//    };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+//        private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-        [Authorize(Roles = AuthorizeRoles.User)]
-        [HttpGet(Name = "GetWeatherForecast")]
-        public string Get()
-        {
-            return "hello world";
-        }
-    }
-}
+//        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+//        {
+//            _logger = logger;
+//        }
+//        [Authorize(Roles = AuthorizeRoles.User)]
+//        [HttpGet(Name = "GetWeatherForecast")] 
+//        public string Get()
+//        {
+//            return "hello world";
+//        }
+//    }
+//}
