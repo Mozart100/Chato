fe2ff9fbc9d20954749e697acced2ba0a24988a9
Arkovean <anatoliy.arkove@gmail.com>
2024-05-15 05:28:25 +0300

    Delegate queque

---
+++ Chato.Server/Infrastracture/DelegateQueue.cs
@@ -0,0 +1,39 @@
+﻿namespace Chato.Server.Infrastracture;
+
+
+public class DelegateQueue
+{
+    private readonly Queue<Func<Task>> _delegates;
+    private readonly CancellationToken _cancellationToken;
+    private readonly Task backgroundTask;
+    private readonly SemaphoreSlim _semaphore;
+
+    public DelegateQueue(CancellationToken cancellationToken)
+    {
+        _cancellationToken = cancellationToken;
+        _delegates = new Queue<Func<Task>>();
+        _semaphore = new SemaphoreSlim(0);
+
+        backgroundTask = Task.Run(async () => await Execute());
+    }
+
+    public async Task Enqueue(Func<Task> callback)
+    {
+        _delegates.Enqueue(callback);
+        _semaphore.Release();
+    }
+
+    public async Task Execute()
+    {
+        while (!_cancellationToken.IsCancellationRequested)
+        {
+            if (_delegates.TryDequeue(out var action))
+            {
+                await action();
+                continue;
+            }
+
+            await _semaphore.WaitAsync();
+        }
+    }
+}
