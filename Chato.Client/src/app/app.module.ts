8dfb8061e73a6c7d4f33458a371507cde0d0fbd7
Arkovean <anatoliy.arkove@gmail.com>
2024-05-25 05:13:43 +0300

    Header+Footer+Content

--- Chato.Client/src/app/app.module.ts
+++ Chato.Client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent,
+    MainFooterComponent,
   ],
   imports: [
     BrowserModule,
