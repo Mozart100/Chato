12f528e616c7c165e3939cde1f5bbdaffd139819
Michael <m.derzhavets@emg-soft.com>
2024-06-28 16:10:51 +0700

    add new client

---
+++ Chato.Client/src/app/chat/components/chat-list-item/chat-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-list-item',
+  templateUrl: './chat-list-item.component.html',
+  styleUrl: './chat-list-item.component.scss'
+})
+export class ChatListItemComponent {
+
+}
---
+++ Chato.Client/src/app/core/services/lang.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core"
+import { TranslateService } from "@ngx-translate/core"
+
+const SELECTED_LANG = "selected.language"
+
+@Injectable({ providedIn: 'root' })
+export class LangService {
+
+    readonly langs = [
+        { text: 'Hebrew', flag: 'assets/images/flags/israel.png', lang: 'he' },
+        { text: 'English', flag: 'assets/images/flags/us.jpg', lang: 'en' },
+        { text: 'Spanish', flag: 'assets/images/flags/spain.jpg', lang: 'es' },
+        { text: 'German', flag: 'assets/images/flags/germany.jpg', lang: 'de' },
+        { text: 'Italian', flag: 'assets/images/flags/italy.jpg', lang: 'it' },
+        { text: 'Russian', flag: 'assets/images/flags/russia.jpg', lang: 'ru' },
+    ]
+
+    constructor(private translate: TranslateService) {
+        this.loadSelectedLanguage()
+    }
+
+    private loadSelectedLanguage() {
+        const lang = localStorage.getItem(SELECTED_LANG)
+        if (lang) {
+            this.setLanguage(lang)
+        }
+    }
+
+    /**
+     * Set language
+     * @param lang language
+     */
+    setLanguage(lang: string) {
+        this.translate.use(lang)
+        this.setDirection(lang)
+    }
+
+    /**
+     * Set page direction
+     * @param lang language
+     */
+    setDirection(lang: string) {
+        document.documentElement.dir = lang === 'he' ? "rtl" : "ltr"
+        localStorage.setItem(SELECTED_LANG, lang)
+    }
+
+
+    get lang() {
+        return this.translate.currentLang
+    }
+}
