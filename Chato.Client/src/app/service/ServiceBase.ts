515e6f4d9fbf75e551f601c9f651ef72250aa344
Michael <m.derzhavets@emg-soft.com>
2024-06-28 16:07:24 +0700

    remove old client

--- Chato.Client/src/app/service/ServiceBase.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-
-
-export class HttpServiceBase {
-  protected baseUrl: string;
-  constructor(protected httpClient: HttpClient) {
-    this.baseUrl = 'https://localhost:7138';
-  }
-}
