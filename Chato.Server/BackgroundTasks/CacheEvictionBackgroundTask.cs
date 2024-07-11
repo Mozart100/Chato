924c283138ece308dc11fac53e722c22e74aebac
Arkovean <anatoliy.arkove@gmail.com>
2024-07-11 21:13:26 +0300

    Adding Configuration for cache

--- Chato.Server/BackgroundTasks/CacheEvictionBackgroundTask.cs
+++ Chato.Server/BackgroundTasks/CacheEvictionBackgroundTask.cs
@@ -1,25 +1,30 @@
-﻿using Chato.Server.DataAccess.Repository;
+﻿using Chato.Server.Configuration;
+using Chato.Server.DataAccess.Repository;
 using Chato.Server.Infrastracture.QueueDelegates;
 using Chato.Server.Services;
+using Microsoft.Extensions.Options;
 
 namespace Chato.Server.BackgroundTasks;
 
 public class CacheEvictionBackgroundTask : BackgroundService
 {
-    private const int Period_Timeout = 1;
-    private const int Timeout_In_Second = 3;
+    //private const int Period_Timeout = 1;
+    //private const int Timeout_In_Second = 3;
+    private readonly EvictionRoomConfig _config;
 
     //private readonly ICacheItemDelegateQueue _cacheItemDelegateQueue;
     private readonly IRoomIndexerRepository _roomIndexerRepository;
     private readonly IServiceScopeFactory _serviceScopeFactory;
     private readonly ILogger<CacheEvictionBackgroundTask> _logger;
 
-    public CacheEvictionBackgroundTask(
+    public CacheEvictionBackgroundTask(IOptions<EvictionRoomConfig> config,
         IRoomIndexerRepository roomIndexerRepository,
         IServiceScopeFactory serviceScopeFactory,
         ILogger<CacheEvictionBackgroundTask> logger
         )
     {
+
+        _config = config.Value;
         this._roomIndexerRepository = roomIndexerRepository;
         this._serviceScopeFactory = serviceScopeFactory;
         this._logger = logger;
@@ -45,7 +50,7 @@ public class CacheEvictionBackgroundTask : BackgroundService
                         continue;
                     }
 
-                    if (timeStemp.IsSecondOver(Timeout_In_Second))
+                    if (timeStemp.IsSecondOver(_config.UnusedTimeoutSeconds))
                     {
                         await roomService.RemoveRoomByNameOrIdAsync(roomName);
                         _logger.LogInformation($"Room {roomName} was evicted!!!.");
@@ -57,7 +62,7 @@ public class CacheEvictionBackgroundTask : BackgroundService
                 }
             }
 
-            await Task.Delay(TimeSpan.FromSeconds(Period_Timeout), stoppingToken);
+            await Task.Delay(TimeSpan.FromSeconds(_config.PeriodTimeoutSeconds), stoppingToken);
         }
     }
 }
---
+++ Chato.Server/Configuration/PQSConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Chato.Server.Configuration;
+
+public class PQSConfig<TClass> where TClass : class
+{
+    public static string ApiName => typeof(TClass).Name;
+
+    public string ConfigurationName => ApiName;
+}
+
+public class EvictionRoomConfig : PQSConfig<EvictionRoomConfig>
+{
+    public int PeriodTimeoutSeconds { get; set; }
+    public int UnusedTimeoutSeconds { get; set; }
+}
