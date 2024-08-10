8e6be744c53cd7de28b138ba05758d9610ef3f2e
Arkovean <anatoliy.arkove@gmail.com>
2024-08-10 14:52:19 +0300

    Adding appsettings infrastructure

--- Chato.Automation/Program.cs
+++ Chato.Automation/Program.cs
@@ -13,7 +13,7 @@ var services = scop.ServiceProvider;
 
 try
 {
-    var app = services.GetRequiredService<App>();
+    var app = services.GetRequiredService<TestPlan>();
     await app.RunAsync(null);
 
 }
@@ -26,50 +26,86 @@ catch (Exception ex)
 
 static IHostBuilder CreateHostBuilder(string[] args)
 {
-    string baseUrl = "http://Chato-1944974610.eu-west-1.elb.amazonaws.com";
+    string baseUrl = null;
 
-    baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
-    if (baseUrl == null)
-    {
-        var localEnvironment = ConfigurationLoader.GetConfigurationRoot(Directory.GetCurrentDirectory());
-        var testPlanSection = localEnvironment.GetSection(TestPlanConfig.ApiName);
-        var testPlanConfig = testPlanSection.Get<TestPlanConfig>();
+    //How to know in what configuration build I run this project ?????
+
+    //if (baseUrl == null)
+    //{
+    //    var localEnvironment = ConfigurationLoader.GetConfigurationRoot(Directory.GetCurrentDirectory(), "Aws");
+    //    var testPlanSection = localEnvironment.GetSection(TestPlanConfig.ApiName);
+    //    var testPlanConfig = testPlanSection.Get<TestPlanConfig>();
 
 
-        baseUrl = testPlanConfig.BaseUrl;
-        //baseUrl = "https://localhost:7138";
-       
-    }
-    var config = new ScenarioConfig
-    {
-        BaseUrl = baseUrl
-    };
+    //    baseUrl = testPlanConfig.BaseUrl;
+    //    //baseUrl = "https://localhost:7138";
 
-    return Host.CreateDefaultBuilder(args)
+    
+    //var config = new ScenarioConfig
+    //{
+    //    BaseUrl = baseUrl
+    //};
+
+    var host = Host.CreateDefaultBuilder(args)
         .ConfigureLogging(logging =>
         {
             //logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Trace);
             //logging.AddFilter("Microsoft.AspNetCore.Http.Connections", LogLevel.Trace);
         })
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            //var env = hostingContext.HostingEnvironment;
+
+            //Console.WriteLine($"Current build configuration: {env.EnvironmentName}");
+
+            //if (env.IsDevelopment())
+            //{
+            //}
+            //else if (env.EnvironmentName == "Aws")
+            //{
+            //}
+        })
 
         .ConfigureServices((_, services) =>
-    {
-        services.AddSingleton<App>();
-        services.AddSingleton<ScenarioConfig>(config);
-        services.AddSingleton<GroupHandShakeScenario>();
-        //services.AddSingleton<HubStreamScenario>();
-        services.AddSingleton<BasicScenario>();
-        services.AddSingleton<RegistrationValidationScenario>();
-        services.AddSingleton<RoomSendingReceivingScenario>();
-        services.AddSingleton<CacheScenario>();
-
-        
+        {
 
 
+            var localEnvironment = ConfigurationLoader.GetConfigurationRoot(Directory.GetCurrentDirectory()); 
+            //var localEnvironment = ConfigurationLoader.GetConfigurationRoot(Directory.GetCurrentDirectory(), "Aws");
+            var testPlanSection = localEnvironment.GetSection(TestPlanConfig.ApiName);
+            var testPlanConfig = testPlanSection.Get<TestPlanConfig>();
+
+
+            baseUrl = testPlanConfig.BaseUrl;
+            //baseUrl = "https://localhost:7138";
+
+
+            var config = new ScenarioConfig
+            {
+                BaseUrl = baseUrl
+            };
 
 
 
-    });
+            services.AddSingleton<TestPlan>();
+            services.AddSingleton<ScenarioConfig>(config);
+            services.AddSingleton<GroupHandShakeScenario>();
+            //services.AddSingleton<HubStreamScenario>();
+            services.AddSingleton<BasicScenario>();
+            services.AddSingleton<RegistrationValidationScenario>();
+            services.AddSingleton<RoomSendingReceivingScenario>();
+            services.AddSingleton<CacheScenario>();
+
+
+
+
+
+
+
+        });
+
+
+    return host;
 }
 
 
