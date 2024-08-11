42be924570a62eeadb8a197516a01f559c77481e
Ariel Gordon <arielgordon123@gmail.com>
2024-08-11 23:17:38 +0300

    test deploy controller

--- Chato.Server/Controllers/ConfigurationController.cs
+++ Chato.Server/Controllers/ConfigurationController.cs
@@ -20,7 +20,13 @@ namespace Chato.Server.Controllers
             _config = config.Value;
             this._mapper = mapper;
         }
-
+        [FeatureGate("ControllerEnabled")]
+        [Route("deploy")]
+        [HttpGet]
+        public async Task<string> Deploy()
+        {
+            return "Its all Good";
+        }
         [Route(EvictionUrl)]
         [HttpGet]
         public async Task<CacheEvictionRoomConfigDto> GetEvictionConfigurationAsync()
